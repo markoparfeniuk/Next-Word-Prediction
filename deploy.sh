457b5961e5954779185c6663d07c58155188efa6
Marko <marko.parfeniuk.pm.2020@lpnu.ua>
2024-02-14 21:15:52 +0200

    update deployment commands

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-echo "Starting deployment script"
-pip install -r requirements.txt && python -m spacy download en_core_web_sm
+echo "Installing Python dependencies from requirements.txt..."
+pip install -r requirements.txt
+
+echo "Downloading the spaCy language model..."
+python -m spacy download en_core_web_sm
