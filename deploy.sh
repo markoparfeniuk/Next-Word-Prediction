1855c4692220bea70aa269fb3993e3464074584d
Marko <marko.parfeniuk.pm.2020@lpnu.ua>
2024-02-14 21:30:14 +0200

    remove synonym search feature

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 echo "Installing Python dependencies from requirements.txt..."
 pip install -r requirements.txt
-
-echo "Downloading the spaCy language model..."
-python -m spacy download en_core_web_sm
